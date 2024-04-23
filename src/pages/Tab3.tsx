8cc376b87be6bddacd378edbf6a0cf64a94d73f6
RicaLucino <20211531@nbsc.edu.ph>
2024-04-23 19:04:05 +0800

    Added source code for clickcounter

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,24 +1,35 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React, { useState } from 'react';
+import { IonContent, IonButton, IonGrid, IonRow, IonCol } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
 
-const Tab3: React.FC = () => {
+const Tab3 = () => {
+  const [count, setCount] = useState(0);
+
+  const incrementCounter = () => {
+    setCount(count + 1);
+  };
+
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
-      </IonContent>
-    </IonPage>
+    <IonContent className="ion-padding">
+      <IonGrid>
+        <IonRow>
+          <IonCol size="12" className="ion-text-center">
+            <h2>Click Counter</h2>
+          </IonCol>
+        </IonRow>
+        <IonRow>
+          <IonCol size="12" className="ion-text-center">
+            <h3>Clicks: {count}</h3>
+          </IonCol>
+        </IonRow>
+        <IonRow>
+          <IonCol size="12" className="ion-text-center">
+            <IonButton onClick={incrementCounter}>Click Me!</IonButton>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
+    </IonContent>
   );
 };
 
